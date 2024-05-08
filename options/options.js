d9fd1674a7a79fec66f45d871334e4d72369a807
Crowdbotics <team@crowdbotics.com>
2024-05-08 11:42:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "boqii-mall-47856",
+  url: "https://boqii-mall-47856.botics.co",
+  api: "https://boqii-mall-47856.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
